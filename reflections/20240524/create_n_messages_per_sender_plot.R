bbc8abf418b9575b3edccda8cd1733a97abb3c52
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-27 10:27:15 +0200

    Analyse chat

--- reflections/20240524/create_n_messages_per_sender_plot.R
+++ reflections/20240524/create_n_messages_per_sender_plot.R
@@ -24,12 +24,24 @@ t$from <- stringr::str_replace(t$from, ".*Saeedeh.*", "L2")
 t$from <- stringr::str_replace(t$from, ".*Sakshi.*", "L3")
 t$from <- stringr::str_replace(t$from, ".*Litika.*", "L4")
 
+n_messages_richel <- nrow(t)
+print(paste("n_messages_richel:", n_messages_richel))
+
+# Only select general messages
+t <- t[t$to == "Everyone", ]
+
+n_messages_richel_to_general <- nrow(t)
+print(paste("n_messages_richel_to_general:", n_messages_richel_to_general))
+n_messages_richel_in_private <- n_messages_richel- n_messages_richel_to_general
+print(paste("n_messages_richel_in_private:", n_messages_richel_in_private))
+
 n_from <- dplyr::tally(dplyr::group_by(t, from))
 
+n_from
 
 ggplot2::ggplot(n_from, mapping = ggplot2::aes(x = from, y = n)) + 
   ggplot2::geom_col() + 
-  ggplot2::scale_y_continuous(name = "Number of chat messages sent") +
+  ggplot2::scale_y_continuous(name = "Number of chat messages sent to everyone") +
   ggplot2::scale_x_discrete(name = "Sender") +
   ggplot2::theme(text = ggplot2::element_text(size = 20)) +
   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
