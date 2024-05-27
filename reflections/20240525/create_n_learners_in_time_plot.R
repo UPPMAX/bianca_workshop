1c10887fc0f7573afddcc3cb8b67a5ddbd6a0ca6
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-27 08:46:16 +0200

    Add mean confidence per session

---
+++ reflections/20240525/create_n_learners_in_time_plot.R
@@ -0,0 +1,33 @@
+#!/bin/env Rscript
+n_learners_in_time <- readr::read_csv(
+  "learners_in_time.csv"
+  )
+
+ggplot2::ggplot(
+  data = n_learners_in_time,
+  mapping = ggplot2::aes(x = time, y = n_learners)
+) + ggplot2::geom_line(
+) + ggplot2::geom_rect(mapping = ggplot2::aes(
+    xmin = readr::parse_time("9:00"), 
+    xmax = readr::parse_time("10:20"), 
+    ymin = 0, 
+    ymax = 8
+  ), fill = ggplot2::alpha("orange", 0.01)
+) + ggplot2::geom_rect(mapping = ggplot2::aes(
+  xmin = readr::parse_time("15:30"), 
+  xmax = readr::parse_time("16:00"), 
+  ymin = 0, 
+  ymax = 8
+), fill = ggplot2::alpha("orange", 0.01)
+) + ggplot2::geom_rect(mapping = ggplot2::aes(
+  xmin = readr::parse_time("12:06"), 
+  xmax = readr::parse_time("13:00"), 
+  ymin = 0, 
+  ymax = 8
+), fill = ggplot2::alpha("blue", 0.01)
+) + ggplot2::theme(text = ggplot2::element_text(size = 20)) +
+  ggplot2::labs(
+  title = "Number of learners in time",
+  caption = "Intermediate Bianca, 2024-05-25, orange = me, blue = break"
+)
+ggplot2::ggsave("n_learners_in_time.png", width = 7, height = 7)
