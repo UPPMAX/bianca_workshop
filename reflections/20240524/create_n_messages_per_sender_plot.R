fd672199c38f1d3dda16db4988498953df9376ce
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-27 10:31:31 +0200

    Label colleagues

--- reflections/20240524/create_n_messages_per_sender_plot.R
+++ reflections/20240524/create_n_messages_per_sender_plot.R
@@ -15,10 +15,10 @@ t <- readr::read_csv(
 )
 names(t) <- c("time", "from", "to")
 t$from <- stringr::str_replace(t$from, ".*Bilderbeek.*", "R")
-t$from <- stringr::str_replace(t$from, ".*Diana.*", "D")
-t$from <- stringr::str_replace(t$from, ".*Lars.*", "L")
-t$from <- stringr::str_replace(t$from, ".*Claremar.*", "B")
-t$from <- stringr::str_replace(t$from, ".*Jonas.*", "J")
+t$from <- stringr::str_replace(t$from, ".*Diana.*", "C1")
+t$from <- stringr::str_replace(t$from, ".*Lars.*", "C2")
+t$from <- stringr::str_replace(t$from, ".*Claremar.*", "C3")
+t$from <- stringr::str_replace(t$from, ".*Jonas.*", "C4")
 t$from <- stringr::str_replace(t$from, ".*Darian.*", "L1")
 t$from <- stringr::str_replace(t$from, ".*Saeedeh.*", "L2")
 t$from <- stringr::str_replace(t$from, ".*Sakshi.*", "L3")
@@ -44,7 +44,8 @@ ggplot2::ggplot(n_from, mapping = ggplot2::aes(x = from, y = n)) +
   ggplot2::scale_y_continuous(name = "Number of chat messages sent to everyone") +
   ggplot2::scale_x_discrete(name = "Sender") +
   ggplot2::theme(text = ggplot2::element_text(size = 20)) +
-  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  ggplot2::labs(caption = "Bianca Intermediate, 2024-05-24")
 
 ggplot2::ggsave("20240524_n_messages_per_sender.png", width = 7, height = 7)
 
